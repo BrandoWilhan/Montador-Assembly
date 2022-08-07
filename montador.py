e2d1ac79897b1c329f269d8dcb70ace87f14063e
Marcos Gabriel O. de Souza <110429686+MarcosUnb@users.noreply.github.com>
2022-08-07 03:19:39 -0300

    Leitor dos arquivos TXT
    Até o momento lê o cabeçalho e guarda os dados das especificações. Porém, pelas funções é possível ler todo o resto do código.

--- montador.py
+++ montador.py
@@ -1,1 +1,27 @@
-print("opa, vamo começar")
+import string
+from turtle import width
+data = open('example_saida_data.mif', 'r')
+text = open('example_saida_text.mif', 'r')
+
+#Seleciona apenas o dado de cada linha(último elemento da string).
+def head(lista, n):
+    piece = lista[n]
+    piece = piece.split()
+    return piece[2]
+
+data.seek(0)
+c = ""
+for linha in data:
+    linha = linha.rstrip()
+    if linha == "CONTENT":
+        break  
+    c = c + linha 
+
+#Guarda dados do Cabecalho
+depth = head(c.split(";"), 0)
+width = head(c.split(";"), 1)
+address_r = head(c.split(";"), 2)
+data_r = head(c.split(";"), 3)  
+print(depth, width, address_r, data_r)
+
+data.close
